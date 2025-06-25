4252566b23acba3be3c7ab4e5457995690bdb43c
Shanto93 <shantoislam7363@gmail.com>
2025-06-25 13:01:03 +0600

    Implement dispatch and fix typescript error

--- redux-learning/src/App.tsx
+++ redux-learning/src/App.tsx
@@ -1,12 +1,27 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import type { RootState } from "./redux/store";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state: RootState) => state.counter.count);
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h2 className="text-xl font-semibold text-amber-950">Counter</h2>
       <div className="flex items-center justify-center mt-4 p-9 bg-white shadow-md rounded-lg">
-        <button className="btn bg-green-500 px-3 py-2 rounded-md text-white font-semibold">Increment</button>
-        <span className="mx-3 font-semibold">0</span>
-        <button className="btn bg-red-500 px-3 py-2 rounded-md text-white font-semibold">Decrement</button>
+        <button
+          onClick={() => dispatch(increment())}
+          className="btn bg-green-500 px-3 py-2 rounded-md text-white font-semibold"
+        >
+          Increment
+        </button>
+        <span className="mx-3 font-semibold">{count}</span>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="btn bg-red-500 px-3 py-2 rounded-md text-white font-semibold"
+        >
+          Decrement
+        </button>
       </div>
     </div>
   );
