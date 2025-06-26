a0d2fe58b6f006117d944b5082411db2a14a267e
Shanto93 <shantoislam7363@gmail.com>
2025-06-26 22:35:02 +0600

    logger

--- redux-learning/src/App.tsx
+++ redux-learning/src/App.tsx
@@ -10,17 +10,29 @@ const App = () => {
       <h2 className="text-xl font-semibold text-amber-950">Counter</h2>
       <div className="flex items-center justify-center mt-4 p-9 bg-white shadow-md rounded-lg">
         <button
-          onClick={() => dispatch(increment())}
+          onClick={() => dispatch(increment(5))}
+          className="btn bg-green-500 px-3 py-2 rounded-md text-white font-semibold mr-3"
+        >
+          Increment 5
+        </button>
+        <button
+          onClick={() => dispatch(increment(1))}
           className="btn bg-green-500 px-3 py-2 rounded-md text-white font-semibold"
         >
           Increment
         </button>
         <span className="mx-3 font-semibold">{count}</span>
         <button
-          onClick={() => dispatch(decrement())}
+          onClick={() => dispatch(decrement(1))}
           className="btn bg-red-500 px-3 py-2 rounded-md text-white font-semibold"
         >
           Decrement
+        </button>
+        <button
+          onClick={() => dispatch(decrement(5))}
+          className="btn bg-red-500 px-3 py-2 rounded-md text-white font-semibold ml-3"
+        >
+          Decrement 5
         </button>
       </div>
     </div>
